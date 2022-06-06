ee4d8fa06d6f61ca372d50c38d38abfcb12fe03c
Olga Stepanova <olichstep@gmail.com>
2022-06-06 10:54:18 +0300

    квадрат числа

---
+++  Seminar1/Program.cs
@@ -0,0 +1,5 @@
+﻿// напишите программу, которая выдает квадрат числа
+Console.WriteLine("введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("число:" + n);
+Console.WriteLine("квадрат числа: " + n * n);
